5276c3c90da2c0627b131fec72f3d90898677169
Gabriel Coelho <51426003+gs-coelho@users.noreply.github.com>
2021-09-08 15:59:45 -0300

    Inicialização do projeto Spring Boot

---
+++ src/main/java/com/tp2/biblioteca/Tp2BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.tp2.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2BibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp2/biblioteca/Tp2BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp2.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
