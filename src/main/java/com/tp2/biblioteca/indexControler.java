8b36100d5cec0b534da371ead307a6afa4e62adb
Bruna Sanches <bcpsanches@gmail.com>
2021-09-09 00:25:50 -0300

    primeira versão do form e o controler.

---
+++ src/main/java/com/tp2/biblioteca/indexControler.java
@@ -0,0 +1,13 @@
+package com.tp2.biblioteca;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexControler {
+
+    @RequestMapping
+    public String index(){
+        return "index";
+    }
+}
