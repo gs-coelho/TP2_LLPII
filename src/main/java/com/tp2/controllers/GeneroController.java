d69c7498a7644746e8fba2ca51e5ae77d2b0f6a0
MariaECarmona <dudacarmona1802@gmil.com>
2021-09-16 23:32:27 -0300

    Update

--- src/main/java/com/tp2/controllers/GeneroController.java
+++ src/main/java/com/tp2/controllers/GeneroController.java
@@ -24,13 +24,13 @@ public class GeneroController {
     @RequestMapping(value = "/genero", method = RequestMethod.POST)
     public String cadastraGenero(@Valid Genero genero, BindingResult result, RedirectAttributes attributes){
         if(result.hasErrors()){
-            attributes.addAttribute("mensagem", "Verifique os campos!");
+            attributes.addAttribute("mensagemValidacao", "Verifique os campos!");
 
             return "redirect:/genero";
         }
 
         gr.save(genero);
-        attributes.addFlashAttribute("mensagem", "Gênero adicionado com sucesso!");
+        attributes.addFlashAttribute("mensagemValidacao", "Gênero adicionado com sucesso!");
 
         return "redirect:/genero";
     }
@@ -55,12 +55,12 @@ public class GeneroController {
     @RequestMapping(value = "/genero/{codigo}", method = RequestMethod.POST)
     public String editarGeneroPost(@PathVariable("codigo") long codigo, @Valid Genero genero, BindingResult result, RedirectAttributes attributes){
         if(result.hasErrors()){
-            attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+            attributes.addFlashAttribute("mensagemValidacao", "Verifique os campos!");
             return "redirect:/genero/{codigo}";
         }
 
         gr.save(genero);
-        attributes.addFlashAttribute("mensagem", "Gênero editado com sucesso!");
+        attributes.addFlashAttribute("mensagemValidacao", "Gênero editado com sucesso!");
 
         return "redirect:/genero/" + codigo;
     }
